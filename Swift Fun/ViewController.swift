d0d71dc65f606acf30ae2502c28a898afad0d0f5
Unknown <patel_anil@hotmail.com>
2018-04-27 21:48:36 +0100

    Now you can add or subtract!

--- Swift Fun/ViewController.swift
+++ Swift Fun/ViewController.swift
@@ -11,13 +11,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var buttonCount = 0
+    // var buttonCount = 0
     @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    @IBOutlet weak var additionSwitch: UISwitch!
+    
     
     @IBAction func buttonTapped(_ sender: Any) {
-        
-        buttonCount = buttonCount + 1
 
+        let addition = additionSwitch.isOn
+
+        if addition {
+            let sum = Double(topTextField.text!)! + Double(bottomTextField.text!)!
+            
+            myLabel.text = "\(topTextField.text!) + \(bottomTextField.text!) = \(sum)"
+
+        } else {
+            let sum = Double(topTextField.text!)! - Double(bottomTextField.text!)!
+            
+            myLabel.text = "\(topTextField.text!) - \(bottomTextField.text!) = \(sum)"
+
+        }
+        
+    
+        print(topTextField.text!)
+        print(bottomTextField.text!)
+        
+
+/*
+        // buttonCount = buttonCount + 1
+        buttonCount += 1
+        
         print(buttonCount)
         
         if buttonCount >= 10 {
@@ -25,9 +50,10 @@ class ViewController: UIViewController {
             view.backgroundColor = UIColor.red
             myLabel.text = "Anil is fuming"
         }
+*/
         
-    }
-    
+         }
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
